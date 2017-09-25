044ecf91a1f1f7f765ed9fa834f9de62ac7a29f9
mosk <miiixsc@gmail.com>
2017-09-25 16:30:13 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 window.onload = function() {
-	var orderText = document.querySelectorAll(".order__text");
+  var order = document.querySelectorAll(".order");
+  var orderText = document.querySelectorAll(".order__text");
+  var orderDate = document.querySelectorAll(".order__text--date");
+  var i;
 
-	console.log(orderText.length);
+  for ( i = 0; i < orderText.length; i++ ) {
+
+    /* скрываем пустые описания */
+    if (orderText[i].innerHTML == "") {
+      orderText[i].style.display="none";
+      var prevText = orderText[i].previousElementSibling;
+      prevText.style.display="none";
+    }
+  }
+
+  for ( i = 0; i < order.length; i++ ) {
+    var text = order[i].querySelector(".order__text--date").innerHTML;
+    var date = new Date(text);
+    console.log(text);
+    console.log(date);
+  }
 }
+
+/*
+  var order = {
+    date: ;
+    condition: ;
+  }
+*/
